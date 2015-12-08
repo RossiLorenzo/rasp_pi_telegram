2e345ecf12a28edc4490b55bd929a0f6a38cb6a7
Lorenzo Rossi <lorenzo.rossi@forward3d.com>
2015-12-08 13:01:14 +0000

    wtr

--- Weather/weather.sh
+++ Weather/weather.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/h$
-  curl "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode=GCDK&NumMins=60" > /home/pi/Lorenzo/Telegram/IrishRail/tmp.txt
-  Rscript /home/pi/Lorenzo/Telegram/IrishRail/live_dart.R
+  Rscript /home/pi/Lorenzo/Telegram/Weather/weather.R
