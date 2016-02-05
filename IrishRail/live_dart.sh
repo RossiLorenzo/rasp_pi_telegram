654609d03d584b6c66d5411d87b97ee92e296746
Lorenzo Rossi <lorereds@gmail.com>
2016-02-05 12:59:22 +0000

    changes sh

--- IrishRail/live_dart.sh
+++ IrishRail/live_dart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/h$
-  curl "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode=GCDK&NumMins=60" > /home/pi/Lorenzo/Telegram/IrishRail/tmp.txt
-  Rscript /home/pi/Lorenzo/Telegram/IrishRail/live_dart.R
+  curl -s "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode=GCDK&NumMins=60" > /home/pi/Lorenzo/Telegram/IrishRail/tmp.txt
+  Rscript /home/pi/Lorenzo/Telegram/IrishRail/live_dart.R >/dev/null
